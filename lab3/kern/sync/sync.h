f667681557f6e5c4c12b5765bda297d0b7a3f7f3
QiuqiuKing777 <2312796@mail.nankai.edu.cn>
2025-10-30 16:53:40 +0800

    Update sync.h
    Answered a question thrown by the comment in the Lab-guidance

--- lab3/kern/sync/sync.h
+++ lab3/kern/sync/sync.h
@@ -18,7 +18,14 @@ static inline void __intr_restore(bool flag) {
         intr_enable();
     }
 }
-
+ /* do...while(0) ensures that no matter what content our macro called,it is compatible with all kind of structure*/
+ /* for xample,when our macro is called in an if-else,while we use a straight-forward grammar below*/
+ /* #define .. x=__intr_save(); ......... and call it in such an content below: */
+ /* if(condition) local_intr_restore(x);else ... ->it would be compiled as*/
+ /* if() ...;; else,there will be two semi,which conflicts the grammar*/
+ /* also,in some more complicated circumstances,when we have more than one sentence in a macro*/
+ /* you'll see:*/
+ /* if() ...; ...; ...; else....; only the first sentence will be judged in the if stmt*/
 #define local_intr_save(x) \
     do {                   \
         x = __intr_save(); \
