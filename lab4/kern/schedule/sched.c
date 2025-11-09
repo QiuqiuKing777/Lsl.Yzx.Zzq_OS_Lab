e2ae98e7448bb83ab917367f71a6ab909fbb9d95
QiuqiuKing777 <2312796@mail.nankai.edu.cn>
2025-11-09 17:40:06 +0800

    Update sched.c

--- lab4/kern/schedule/sched.c
+++ lab4/kern/schedule/sched.c
@@ -3,7 +3,7 @@
 #include <proc.h>
 #include <sched.h>
 #include <assert.h>
-
+//fifo strategy proc schedule
 void
 wakeup_proc(struct proc_struct *proc) {
     assert(proc->state != PROC_ZOMBIE && proc->state != PROC_RUNNABLE);
@@ -15,27 +15,27 @@ schedule(void) {
     bool intr_flag;
     list_entry_t *le, *last;
     struct proc_struct *next = NULL;
-    local_intr_save(intr_flag);
+    local_intr_save(intr_flag);//after this,id intr_flag=1,means trap disabled
     {
-        current->need_resched = 0;
+        current->need_resched = 0;//clear need reschedule flag,avoid resched again
+        //if nowa idle:start search from head,else from next proc of temp
         last = (current == idleproc) ? &proc_list : &(current->list_link);
         le = last;
         do {
             if ((le = list_next(le)) != &proc_list) {
                 next = le2proc(le, list_link);
-                if (next->state == PROC_RUNNABLE) {
+                if (next->state == PROC_RUNNABLE) {//ve founded the one
                     break;
                 }
             }
         } while (le != last);
         if (next == NULL || next->state != PROC_RUNNABLE) {
-            next = idleproc;
+            next = idleproc;//not runnable found
         }
-        next->runs ++;
+        next->runs ++;//count++
         if (next != current) {
-            proc_run(next);
+            proc_run(next);//run the founded runnable proc
         }
     }
     local_intr_restore(intr_flag);
 }
-
