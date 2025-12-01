31eb9f7121fd44aab5f4d9f5c37d332399a30bbf
Zhongxiu Yang <2432843806@qq.com>
2025-12-01 10:29:28 +0800

    Update IRQ_S_TIMER case in trap.c
    Refactor supervisor timer interrupt handling to set reschedule flag.

--- lab5/kern/trap/trap.c
+++ lab5/kern/trap/trap.c
@@ -120,25 +120,10 @@ void interrupt_handler(struct trapframe *tf)
         cprintf("User software interrupt\n");
         break;
     case IRQ_S_TIMER:
-        // "All bits besides SSIP and USIP in the sip register are
-        // read-only." -- privileged spec1.9.1, 4.1.4, p59
-        // In fact, Call sbi_set_timer will clear STIP, or you can clear it
-        // directly.
-        // cprintf("Supervisor timer interrupt\n");
-        /* LAB3 EXERCISE1   2312323 :  */
-        /*(1)设置下次时钟中断- clock_set_next_event()
-         *(2)计数器（ticks）加一
-         *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
-         * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
-         */
         clock_set_next_event();
         ticks++;
-        if(ticks%TICK_NUM==0){
-            print_ticks();
-            num++;
-            if(num>=10){
-                sbi_shutdown();
-            }
+        if(ticks%TICK_NUM==0&&current){
+           current->need_resched=1;
         }
         break;
     case IRQ_H_TIMER:
